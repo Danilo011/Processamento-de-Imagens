6e96494015d8fee4c23d4dd54b9b1723bb295378
Danilo011 <88005239+Danilo011@users.noreply.github.com>
2025-09-02 16:23:12 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,131 @@
+import cv2
+import numpy
+import matplotlib.pyplot as plt
+
+
+def dadosImagem(imagem):
+    print("\nLargura em pixels: ", end='')
+    print(imagem.shape[1]) # Largura da imagem
+    print("Altura em pixels: ", end='')
+    print(imagem.shape[0]) # Altura da imagem
+    print("Qtde de canais: ", imagem.shape[2]) # Quantidade de Canais da Imagem - Imagem colorida possui 3 canais RGB
+    (b, g, r) = imagem[0,0] # RGB de pixel especifico
+    print("cor RGB de pixel especifico:", end="")
+    print("Azul: ", b , end="")
+    print(" Verde: ", g,  end="")
+    print(" Vermelho: ", r )
+    print("Corpo", imagem.shape)
+    print("Tamanho", imagem.size)
+    print("Número de dimensões", imagem.ndim)
+
+
+def separarCamada(imagem):
+    #Cria uma matriz do tamanho da imagem contendo somente 0
+    canalBlue = numpy.zeros((imagem.shape[0], imagem.shape[1], imagem.shape[2]), dtype=numpy.uint8)     
+    canalGreen = numpy.zeros((imagem.shape[0], imagem.shape[1], imagem.shape[2]), dtype=numpy.uint8)
+    canalRed = numpy.zeros((imagem.shape[0], imagem.shape[1], imagem.shape[2]), dtype=numpy.uint8)
+
+    canalBlue[:,:,0] = imagem[:,:,0] #Copia o canal azul para a nova matriz
+    canalGreen[:,:,1] = imagem[:,:,1]
+    canalRed[:,:,2] = imagem[:,:,2]
+
+    return canalBlue[:,:,0] , canalGreen[:,:,1], canalRed[:,:,2]
+
+def transformarCinza(imagem):
+    canalGray = numpy.zeros((imagem.shape[0], imagem.shape[1]), dtype=numpy.uint8)
+    for i in  range(imagem.shape[0]):
+        for j in range (imagem.shape[1]):
+            canalGray[i,j] = int(imagem[i, j].sum() // 3)
+    return canalGray
+
+def histograma(imagem, cor):
+    pixel = 256*[0] #Define eixo x
+    for i in range(256):
+        pixel[i]=i
+
+    plt.xlabel('Pixel')  #Nome eixo x
+    plt.ylabel('Quantidade') #Nome eixo y
+    plt.title('Histograma da imagem') #Titulo do plot
+
+    histograma = numpy.zeros(256, dtype=int) #Cria o histograma da imagem
+    for i in  range(imagem.shape[0]):
+        for j in range (imagem.shape[1]):
+            histograma[imagem[i,j]] += 1
+
+    plt.bar(pixel, histograma, color = cor)
+    plt.show()
+
+# Codigo do professor
+def hist(imagem, canal):
+    pixel = 256*[0] #Define eixo x
+    for i in range(256):
+        pixel[i]=i
+
+    plt.xlabel('Pixel')  #Nome eixo x
+    plt.ylabel('Quantidade') #Nome eixo y
+    plt.title('Histograma da imagem') #Titulo do plot
+
+    histograma = numpy.zeros(256, dtype=numpy.uint8)
+    for i in range(imagem.shape[0]):
+        for j in range(imagem.shape[1]):
+            histograma[imagem[i][j]] += 1
+
+    plt.bar(pixel, histograma, color = canal)
+    plt.show()
+
+def limiar(imagem, ponto, modo):
+    copia = numpy.zeros((imagem.shape[0], imagem.shape[1]), dtype=numpy.uint8)
+    for i in  range(imagem.shape[0]):
+        for j in range (imagem.shape[1]):
+            if modo == 1: # Remove parte clara
+                if imagem[i, j] > ponto:
+                    copia[i, j] = 255
+                else:
+                    copia[i, j] = imagem[i, j]
+            else: # Remove parte escura
+                if imagem[i, j] > ponto:
+                    copia[i, j] = imagem[i, j]
+                else:
+                    copia[i, j] = 255
+    return copia
+
+def limiar3ton(imagem, p1, p2):
+    copia = numpy.zeros((imagem.shape[0], imagem.shape[1]), dtype=numpy.uint8)
+    for i in  range(imagem.shape[0]):
+        for j in range (imagem.shape[1]):
+                if imagem[i, j] < p1: # Remove antes do primeiro ponto
+                    copia[i, j] = 255
+                if imagem[i, j] > p1 and imagem[i, j] < p2: # Remove entre o primeiro ponto e o segundo
+                    copia[i, j] = 255
+                if (imagem[i, j] > p2): # Remove depois do segundo ponto
+                    copia[i, j] = 255
+                else:
+                    copia[i, j] = imagem[i, j]
+    return copia
+def main():
+    imagem = cv2.imread("imagens/3t.png")
+
+    dadosImagem(imagem)
+    canalBlue, canalGreen, canalRed = separarCamada(imagem)
+    canalGray = transformarCinza(imagem)
+    histograma(canalGray, "Gray")
+    #cv2.imshow("CINZA",canalGray)
+    #histograma(canalBlue, "Blue")
+    #histograma(canalGreen, "Green")
+    #histograma(canalRed, "Red")
+    #hist(imagem[:,:,0], "Blue") # Codigo do Professor
+    #hist(imagem[:,:,1], "Green") # Codigo do Professor
+    #hist(imagem[:,:,2], "Red") # Codigo do Professor
+    #cv2.imshow("Canal Blue", canalBlue)
+    #cv2.imshow("Canal Green", canalGreen)
+    #cv2.imshow("Canal Red", canalRed)
+    #cv2.imshow("Canal gray", canalGray)
+    #limiarizada = limiar(canalGray, 130, 1)
+    limiarizada = limiar3ton(canalGray, 100, 200)
+    cv2.imshow("Imagem Limiarizada", limiarizada)
+    cv2.imwrite("imagens/saida.jpg", imagem)
+    cv2.imwrite("imagens/saida.jpg", canalGray)
+    cv2.waitKey(0)
+
+if __name__ =='__main__':
+    main()
